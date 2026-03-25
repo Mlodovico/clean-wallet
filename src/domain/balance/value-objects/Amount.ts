f37a6c30548209265b0edafaf8bacdc18d55204a
Mlodovico <murilo.lodovico@outlook.com>
2026-03-24 22:44:20 -0300

    feature: Amount VO and custom errors

--- src/domain/balance/value-objects/Amount.ts
+++ src/domain/balance/value-objects/Amount.ts
@@ -1,16 +1,41 @@
+import { amountErrors } from '../errors/amountErrors.errors';
 
 export class Amount {
-    private constructor(private readonly value: number) {}
+  private constructor(private readonly value: number) {}
 
-    static create(amount: number): Amount {
-        if (amount <= 0) {
-            throw new Error('Amount must be greater than 0');
-        }
-
-        return new Amount(amount);
+  static create(amount: number): Amount {
+    if (amount <= 0) {
+      throw amountErrors.amountMustBeGreaterThanZero();
     }
 
-    getValue(): number {
-        return this.value;
+    if (!Amount.isFinite(amount)) {
+      throw amountErrors.amountMustBeFinite();
     }
+
+    if (!Amount.hasAtMostTwoDecimals(amount)) {
+      throw amountErrors.amountMustHaveAtMostTwoDecimals();
+    }
+
+    return new Amount(amount);
+  }
+
+  static isPositive(value: number): boolean {
+    return value > 0;
+  }
+
+  static isNegative(value: number): boolean {
+    return value < 0;
+  }
+
+  static hasAtMostTwoDecimals(value: number): boolean {
+    return Number.isInteger(value * 100);
+  }
+
+  static isFinite(value: number): boolean {
+    return Number.isFinite(value);
+  }
+
+  getValue(): number {
+    return this.value;
+  }
 }
