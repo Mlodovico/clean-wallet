6a3b9e026d34d26a47436c8511cc24f00d30adbe
Mlodovico <murilo.lodovico@outlook.com>
2026-05-03 23:47:30 -0300

    feature: creating controller methods

--- src/presentation/client/client.controller.ts
+++ src/presentation/client/client.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put } from '@nestjs/common';
 
 import { ClientService } from './client.service';
 import { Client } from '../../domain/client/Client';
@@ -11,5 +11,20 @@ export class ClientController {
   @Get()
   findAll(): Client[] {
     return this.clientService.findAll();
+  }
+
+  @Post()
+  CreateNewClient(newClient: Client): Client {
+    return this.clientService.create(newClient);
+  }
+
+  @Put(':id')
+  updateClient(id: string, updateData: Partial<Client>): Client | undefined {
+    return this.clientService.update(id, updateData);
+  }
+
+  @Get(':id')
+  findById(id: string): Client | undefined {
+    return this.clientService.findOne(id);
   }
 }
